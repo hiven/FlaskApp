9dc905f6321383070fdd21710dc2d236272697c8
hiven <mail@hiven.com>
2021-12-19 09:03:57 +0000

    v

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(config_class=Config):
 
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+#not sure if i mucked this up
     db.init_app(app)
 
     from app.main import main_blueprint
