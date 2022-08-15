8f1d0ce057e2e00a7790e2226da4d214336480e2
hiven <mail@hiven.com>
2022-08-15 15:10:29 +0100

    Update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,7 @@ def all_items():
 @main_blueprint.route('/view/<items_id>', methods=['GET', 'POST'])
 def view_items(items_id):
     view_user_items = Items.query.filter_by(id=items_id)
-    return render_template('main/item.html', items=view_user_items)
+    return render_template('main/items.html', items=view_user_items)
 
 @main_blueprint.route('/add', methods=['GET', 'POST'])
 def add_item():
